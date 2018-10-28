edb10a7e9089d18b52f8e4057029772baee11b9a
Хорошилов Павел Сергеевич <PSKhoroshilov.SBT@sberbank.ru>
2018-10-28 16:47:55 +0300

    prepare images

--- makeGallery.js
+++ makeGallery.js
@@ -1,65 +1,136 @@
-var path = require("path");
-var recursive = require("recursive-readdir");
-var rimraf = require("rimraf");
-var mkdirp = require('mkdirp');
-var crypto = require('crypto');
+const path = require("path");
+const recursive = require("recursive-readdir");
+const rimraf = require("rimraf");
+const mkdirp = require('mkdirp');
+const crypto = require('crypto');
+const transliteration = require('transliteration');
+const slugify = transliteration.slugify;
+const sharp = require('sharp');
+const TextToSVG = require('text-to-svg');
+const textToSVG = TextToSVG.loadSync();
 
-var rootDir = path.join("E:", "photo", "for_site");
-var galleryDir = path.join(__dirname, "static", "images");
 
-rimraf.sync(galleryDir);
-mkdirp.sync(galleryDir);
+const attributes = {
+    fill: 'white',
+    'fill-opacity': ".3",
+};
+const options = {x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes};
 
-var ORIGINAL = "original";
-var PREVIEW = "preview";
+const watermarkLogo = textToSVG.getSVG('khoroshilova.pro', options);
+
+// For Win
+// const rootDir = path.join("E:", "photo", "for_site");
+// For Mac
+const rootDir = path.join("/", "Users", "pavel", "for_site");
+const staticDir = path.join(__dirname, "static");
+const imagesDir = path.join(staticDir, "images");
+
+// rimraf.sync(imagesDir);
+// mkdirp.sync(imagesDir);
+
+const ORIGINAL = "original";
+const PREVIEW = "preview";
 
 function prepareFileInfo(originalPath, index) {
-    var dir = originalPath.replace(rootDir, "").split(path.sep);
+    const dir = originalPath.replace(rootDir, "").split(path.sep);
 
     dir.shift();
 
-    var fileName = dir.pop();
+    const originalFileName = dir.pop().split(".");
+    const fileName = `${slugify(originalFileName[0])}.${originalFileName[1]}`;
+    const parentDirs = dir.slice(0, -1);
+
+    const pathForHash = parentDirs.join("-");
+    const hashDir = crypto.createHash('md5').update(pathForHash).digest("hex").substring(0, 5);
+
+    const directoryName = `${hashDir}-${slugify(dir[dir.length - 1])}`;
 
     return {
         id: index,
         originalPath: originalPath,
-        // dirHash: crypto.createHash('md5').update(dir.join("-")).digest("hex"),
+        hashDir,
+        directoryName,
         dir: dir,
-        fileName: fileName
+        originalFileName,
+        fileName,
+        path: {
+            preview: path.join("images", PREVIEW, directoryName, fileName),
+            original: path.join("images", ORIGINAL, directoryName, fileName),
+        }
     };
 }
 
+const IGNORE_FILES = [".DS_Store"];
 
-
-recursive(rootDir, function (err, files) {
-
-    var filesInfo = files.map(prepareFileInfo);
-
-    var dirNames = filesInfo.reduce((dirs, fileInfo) => {
-
-        fileInfo.dir.forEach((dirName, index) => {
-            var c = crypto.createHash('md5').update(fileInfo.dir.slice(index).join("-")).digest("hex")
-            var checkName = c + "-" + dirName;
-            var level = dirs.indexOf(dirName);
-
-            if (level < 0) {
-                console.log(checkName);
-                dirs.push(dirName);
-            }
-        })
+function createDirectories(filesInfo) {
+    const dirNames = filesInfo.reduce((dirs, fileInfo) => {
+        if (dirs.indexOf(fileInfo.directoryName) < 0) {
+            dirs.push(fileInfo.directoryName);
+        }
 
         return dirs;
-    }, [])
+    }, []);
 
     dirNames.forEach((dirName) => {
-        mkdirp.sync(path.join(galleryDir, ORIGINAL, dirName))
-        mkdirp.sync(path.join(galleryDir, PREVIEW, dirName))
+        mkdirp.sync(path.join(imagesDir, ORIGINAL, dirName));
+        mkdirp.sync(path.join(imagesDir, PREVIEW, dirName));
+    });
+
+    console.log(dirNames);
+}
+
+function copyImages(filesInfo) {
+
+    let promiseArray = [];
+
+    filesInfo.forEach((fileInfo) => {
+
+        const pre = new Promise((resolve) => {
+            sharp(fileInfo.originalPath)
+                .resize(200)
+                .toFile(path.join(staticDir, fileInfo.path.preview))
+                .then(() => {
+                    console.log('done pre', fileInfo.path.preview);
+                    resolve()
+                });
+        });
+
+
+        const ori = new Promise((resolve) => {
+            sharp(fileInfo.originalPath)
+                .withMetadata()
+                .resize(960)
+                .overlayWith(new Buffer(watermarkLogo), {gravity: sharp.gravity.southeast})
+                .toFile(path.join(staticDir, fileInfo.path.original))
+                .then(() => {
+                    console.log('done ori', fileInfo.path.original);
+                    resolve()
+                });
+        });
+
+        promiseArray.push(pre, ori)
+
+    });
+
+    Promise.all(promiseArray).then(() => {
+        console.log('done all')
     })
 
-    // console.log(filesInfo);
-    // `files` is an array of file paths
-    console.log(dirNames);
+}
 
+recursive(rootDir, IGNORE_FILES, function (err, files) {
+
+    if (err) {
+        console.error('err', err);
+        return;
+    }
+
+    const filesInfo = files.map(prepareFileInfo);
+
+
+    // createDirectories(filesInfo);
+    // copyImages(filesInfo);
 });
+
 
 
